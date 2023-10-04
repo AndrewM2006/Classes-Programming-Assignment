f1b69273c5a3639fcfda993ed4796ec403f21ea5
Andrew Monteith <339531824@GoTVDSB.ca>
2023-10-04 10:54:45 -0400

    this sucks

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace Classes_Programming_Assignment
     {
         static void Main(string[] args)
         {
-
+            List <string> Class = new List<string>();
+            Student kid = new Student("George", "Washington");
+            Class.Add(kid.FirstName + " " + kid.LastName);
+            kid.FirstName = "John"; kid.LastName = "Doe";
+            Class.Add(kid.FirstName + " " + kid.LastName);
+            kid.FirstName = "Jack"; kid.LastName = "Eitel";
+            Class.Add(kid.FirstName + " " + kid.LastName);
+            foreach (string s in Class)
+            {
+                Console.WriteLine(s);
+            }
+            Class.Clear();
+            for (int i  = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter a first name and then a last name...");
+                kid.FirstName=Console.ReadLine();
+                kid.LastName=Console.ReadLine();
+                Class.Add(kid.FirstName + " " + kid.LastName);
+            }
+            foreach (string s in Class)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -8,5 +8,78 @@ namespace Classes_Programming_Assignment
 {
     internal class Student
     {
+        private static Random Generator = new Random();
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private int _studentNum;
+
+        public Student (string firstName, string lastName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            _studentNum = Generator.Next(1000) + 555000;
+            GenerateEmail();
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                this._firstName = value;
+                GenerateEmail();
+
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                this._lastName = value;
+                GenerateEmail();
+
+            }
+        }
+
+        public int StudentNumber
+        {
+            get
+            {
+                return _studentNum;
+            }
+        }
+
+        public override string ToString()
+        {
+            return _firstName + " " + _lastName;
+        }
+
+        public void ResetNum()
+        {
+            _studentNum = Generator.Next(1000) + 555000;
+            GenerateEmail();
+        }
+
+        private void GenerateEmail()
+        {
+            _email = _firstName.Substring(0,3) + _lastName.Substring(0,3) + _studentNum.ToString().Substring(_studentNum.ToString().Length-3) + "@ICS4U.com";
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+        }
     }
 }
